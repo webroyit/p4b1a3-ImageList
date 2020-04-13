46fd5de4fb774764543070aedbdf16d77c863d02
royweb <xytank@gmail.com>
2020-04-13 14:11:50 -0400

    Use AppBar to add navbar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from 'react';
+// MuiThemeProvider makes the material-ui work
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import NavBar from './components/navbar/NavBar';
+
 import './App.css';
 
 function App() {
   return (
-    <>
-      <h1>Image List</h1>
-    </>
+    <MuiThemeProvider>
+      <NavBar />
+    </MuiThemeProvider>
   );
 }
 
