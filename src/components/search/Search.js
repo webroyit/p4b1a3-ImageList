5ff6579e43d2e2010cd8a988ed0064187b57143c
royweb <xytank@gmail.com>
2020-04-14 10:38:11 -0400

    Add select field and menu item from material-ui

--- src/components/search/Search.js
+++ src/components/search/Search.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import TextField from 'material-ui/TextField';
 import SelectField from 'material-ui/SelectField';
+import MenuItem from 'material-ui/MenuItem';
 
 import {APIKEY} from "../../Config";
 
@@ -22,6 +23,20 @@ class Search extends Component{
                     onChange={this.onTextChange}
                     floatingLabelText="Search for Images"
                     fullWidth />
+
+                <br />
+                <SelectField
+                    name="amount"
+                    floatingLabelText="Amount"
+                    value={this.state.amount}
+                    onChange={this.onAmountChange}
+                >
+                    <MenuItem value={5} primaryText="5" />
+                    <MenuItem value={10} primaryText="10" />
+                    <MenuItem value={15} primaryText="15" />
+                    <MenuItem value={30} primaryText="30" />
+                    <MenuItem value={50} primaryText="50" />
+                </SelectField>
             </>
         )
     }
