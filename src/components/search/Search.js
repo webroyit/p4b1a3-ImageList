0f1502b47311fc689bf68ef8e87d60aff4a7e965
royweb <xytank@gmail.com>
2020-04-15 13:00:48 -0400

    Hide the images when the search text input is empty

--- src/components/search/Search.js
+++ src/components/search/Search.js
@@ -18,12 +18,18 @@ class Search extends Component{
 
     // arrow function bind this
     onTextChange = e => {
-        this.setState({ [e.target.name]: e.target.value }, () => {
-            axios
-                .get(`${this.state.apiUrl}/?key=${this.state.apiKey}&q=${this.state.searchText}
-            &image_type=photo&per_page=${this.state.amount}&safesearch=true`)
-                .then(res => this.setState({images: res.data.hits}))
-                .catch(err => console.log(err));
+        const val = e.target.value;
+
+        this.setState({ [e.target.name]: val }, () => {
+            if(val === ""){
+                this.setState({ images: []});
+            }else{
+                axios
+                    .get(`${this.state.apiUrl}/?key=${this.state.apiKey}&q=${this.state.searchText}
+                &image_type=photo&per_page=${this.state.amount}&safesearch=true`)
+                    .then(res => this.setState({images: res.data.hits}))
+                    .catch(err => console.log(err));
+            }
         })
     }
 
