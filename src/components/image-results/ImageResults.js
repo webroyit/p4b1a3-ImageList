f2d7ddb1c35f5b0a4aa6db661bf9ebef6a4c694b
royweb <xytank@gmail.com>
2020-04-15 12:24:10 -0400

    Use GridList and GridTile from material-ui to display a grid for images

--- src/components/image-results/ImageResults.js
+++ src/components/image-results/ImageResults.js
@@ -1,12 +1,31 @@
 import React, { Component } from 'react';
 import PropTypes from "prop-types";
+import { GridList, GridTile } from 'material-ui/GridList';
 
 class ImageResults extends Component{
     render(){
-        console.log(this.props.images);
+        const { images } = this.props;
+        let imageListContent;
+
+        if(images){
+            imageListContent = (
+                <GridList cols={3}>
+                    {images.map(img => (
+                        <GridTile
+                            title={img.tags}
+                            key={img.id}
+                            >
+                                <img src={img.largeImageURL} alt="Images" />
+                        </GridTile>
+                    ))}
+                </GridList>
+            )
+        } else{
+            imageListContent = null;
+        }
         return(
             <div>
-                <h2>Images</h2>
+                {imageListContent}
             </div>
         )
     }
